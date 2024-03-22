1a20bdbf6bad101a71b7f074c1b1e5d7180d53de
Gerardo Valenzuela <gerardov@colostate.edu>
2024-03-22 14:36:28 -0600

    Defined about.php  and a basic layout

---
+++ about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<h1>About Us</h1>
+
+<h3>Group Member Name</h3>
+<img src = "" alt="Image go here">
+<p>Bio here</p>
+
+<h3>Group Member Name</h3>
+<img src = "" alt="Image go here">
+<p>Bio here</p>
+
+
+</html>
