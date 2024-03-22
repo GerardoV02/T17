9d939f6c469560b43bbb32a3adcd4b773c5dd81f
Gerardo Valenzuela <gerardov@colostate.edu>
2024-03-22 14:39:01 -0600

    Added head

--- about.php
+++ about.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<head>
+    <title>Homepage</title>
+    <meta charset="UTF-8">
+    <meta name = "author" content = "Gerardo Valenzuela">
+    <meta name = description content = "About Page for Group Project Website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <html>
 
 <h1>About Us</h1>
