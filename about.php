c956b9b8829d8575a1d8a45aed8d82a2d96dc122
Gerardo Valenzuela <gerardov@colostate.edu>
2024-03-22 15:35:46 -0600

    Small changes like body tag and moving items

--- about.php
+++ about.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+
+<html>
+
 <head>
     <title>About Us</title>
     <meta charset="UTF-8">
@@ -6,7 +9,6 @@
     <meta name = description content = "About Page for Group Project Website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<html>
 
 <!--
 Classes:
@@ -15,26 +17,26 @@ Classes:
 - Images = "MemberPic"
 - Bio paragraphs = "Bio"
 -->
+<body>
+    <h1 class = "PageTitle">About Us</h1>
 
-<h1 class = "PageTitle">About Us</h1>
+    <h3 class = "MemberName">Gerardo Valenzuela</h3>
+    <img class = "MemberPic" src = "Images/gerardo.jpg" alt="Image go here" width = 400px>
+    <p class = "Bio">Hi there! My name is Gerardo Valenzuela and I am a 3rd year student.</p>
+    <p class = "Bio">Some fun facts about me are I like cars, I'm a pretty good cook, and I'm bilingual.</p>
 
-<h3 class = "MemberName">Gerardo Valenzuela</h3>
-<img class = "MemberPic" src = "Images/gerardo.jpg" alt="Image go here" width = 400px>
-<p class = "Bio">Hi there! My name is Gerardo Valenzuela and I am a 3rd year student.</p>
-<p class = "Bio">Some fun facts about me are I like cars, I'm a pretty good cook, and I'm bilingual.</p>
+    <h3 class = "MemberName">Group Member Name</h3>
+    <img class = "MemberPic" src = "" alt="Image go here">
+    <p class = "Bio">Bio here</p>
 
-<h3 class = "MemberName">Group Member Name</h3>
-<img class = "MemberPic" src = "" alt="Image go here">
-<p class = "Bio">Bio here</p>
+    <h3 class = "MemberName">Group Member Name</h3>
+    <img class = "MemberPic" src = "" alt="Image go here">
+    <p class = "Bio">Bio here</p>
 
-<h3 class = "MemberName">Group Member Name</h3>
-<img class = "MemberPic" src = "" alt="Image go here">
-<p class = "Bio">Bio here</p>
-
-<h3 class = "MemberName">Group Member Name</h3>
-<img class = "MemberPic" src = "" alt="Image go here">
-<p class = "Bio">Bio here</p>
+    <h3 class = "MemberName">Group Member Name</h3>
+    <img class = "MemberPic" src = "" alt="Image go here">
+    <p class = "Bio">Bio here</p>
+</body>
 
 <footer>Copyright Info Here</footer>
-
 </html>
