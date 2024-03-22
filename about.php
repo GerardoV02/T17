195acdc67c0a98d9ea9d975d4c32d2329adb585a
Gerardo Valenzuela <gerardov@colostate.edu>
2024-03-22 15:26:44 -0600

    Added classes to About page and images as well

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <title>Homepage</title>
+    <title>About Us</title>
     <meta charset="UTF-8">
     <meta name = "author" content = "T17">
     <meta name = description content = "About Page for Group Project Website">
@@ -8,14 +8,33 @@
 </head>
 <html>
 
-<h1>About Us</h1>
+<!--
+Classes:
+- Title = "PageTitle"
+- Name = "MemberName"
+- Images = "MemberPic"
+- Bio paragraphs = "Bio"
+-->
 
-<h3>Group Member Name</h3>
-<img src = "" alt="Image go here">
-<p>Bio here</p>
+<h1 class = "PageTitle">About Us</h1>
 
-<h3>Group Member Name</h3>
-<img src = "" alt="Image go here">
-<p>Bio here</p>
+<h3 class = "MemberName">Gerardo Valenzuela</h3>
+<img class = "MemberPic" src = "Images/gerardo.jpg" alt="Image go here" width = 400px>
+<p class = "Bio">Hi there! My name is Gerardo Valenzuela and I am a 3rd year student.</p>
+<p class = "Bio">Some fun facts about me are I like cars, I'm a pretty good cook, and I'm bilingual.</p>
+
+<h3 class = "MemberName">Group Member Name</h3>
+<img class = "MemberPic" src = "" alt="Image go here">
+<p class = "Bio">Bio here</p>
+
+<h3 class = "MemberName">Group Member Name</h3>
+<img class = "MemberPic" src = "" alt="Image go here">
+<p class = "Bio">Bio here</p>
+
+<h3 class = "MemberName">Group Member Name</h3>
+<img class = "MemberPic" src = "" alt="Image go here">
+<p class = "Bio">Bio here</p>
+
+<footer>Copyright Info Here</footer>
 
 </html>
