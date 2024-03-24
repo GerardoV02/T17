15133e5a82182635f21ffce6263f35d08cb3bc49
Tyler Carver <carv@barracuda.cs.colostate.edu>
2024-03-23 18:15:49 -0600

    added my about bio to the about.php file

--- about.php
+++ about.php
@@ -23,9 +23,10 @@ Classes:
 <p class = "Bio">Hi there! My name is Gerardo Valenzuela and I am a 3rd year student.</p>
 <p class = "Bio">Some fun facts about me are I like cars, I'm a pretty good cook, and I'm bilingual.</p>
 
-<h3 class = "MemberName">Group Member Name</h3>
-<img class = "MemberPic" src = "" alt="Image go here">
-<p class = "Bio">Bio here</p>
+<h3 class = "MemberName">Tyler Carver</h3>
+<img class = "MemberPic" src = "Images/tyler.jpg" alt="Image go here" width = 400px>
+<p class = "Bio">Hello! My Name is Tyler Carver and I am a 3rd year student.</p>
+<p class = "Bio">Some cool facts about me: I used to work at a race track, I also like cars, and love halloween</p>
 
 <h3 class = "MemberName">Group Member Name</h3>
 <img class = "MemberPic" src = "" alt="Image go here">
