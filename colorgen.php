ad29139e2cf87e34f0d9e96943c53a9fe1be6966
Tyler Carver <carv@barracuda.cs.colostate.edu>
2024-03-25 12:35:53 -0600

    added a template for the organization of files in index.php

---
+++ colorgen.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <title>Color generator</title>
+    <meta charset="UTF-8">
+    <meta name = "author" content = "T17">
+    <meta name = description content = "Color generator page for Group Project Website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<html>
+<h1> color generator page </h1>
+</html>
---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name = "author" content = "T17">
+    <meta name = description content = "Home page for Group Project Website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<html>
+<h1>homepage!</h1>
+</html>
