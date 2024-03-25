731a4849c121830c0087684c14fbc5978f711236
Andrew Partain <andrew.partain@colostate.edu>
2024-03-25 13:44:42 -0600

    Moved <body> to index to reduce num times used

--- home.php
+++ home.php
@@ -1,10 +1,5 @@
-<body>
-    <!--
-    Classes:
-    - Title="PageTitle"
-    - Name="MemberName"
-    - Images="MemberPic"
-    - Bio paragraphs="Bio"
-    -->
-    <h1 class="PageTitle"><?php echo ucwords($page);?></h1>
-</body>
+<!--
+Classes:
+- Title="PageTitle"
+-->
+<h1 class="PageTitle"><?php echo ucwords($page);?></h1>
