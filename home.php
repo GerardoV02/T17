5c2a7385540e897d241868cad533eed004016de0
Josh1302 <josh1302@colostate.edu>
2024-04-02 15:37:51 -0600

    Added information to homepage

--- home.php
+++ home.php
@@ -1,5 +1,19 @@
 <!--
 Classes:
 - Title="PageTitle"
+- Images="TeamLogo"
+- MissionStatement="Mission Statement"
+- Statement="statement"
+- Description="description"
+- Paragraph="description of colorgenerator"
 -->
 <h1 class="PageTitle"><?php echo ucwords($page);?></h1>
+
+<img class="TeamLogo" src="Images/logo.jpeg" width="400px" height="400px">
+
+<h3 class="Mission Statement">Our Mission Statement</h3>
+<p class="statement">To embower those through our web design, while also creating a meaningful website for those to interact with. We are building a unique way for those to generate colors that has rarely been seen before. Welcome to Chroma 17, we are glad to have you!</p>
+
+<h3 class="description">Description Of Our Prototype</h3>
+<p class="description of colorgenerator">Our color generator allows users to create a unique table based on there own inputs. First the user specifies the dimension and the number if unique colors they would like to include. Once this is set the user then selects the each color they would like to use.
+This input is validated to make sure there are no repeats as each color must be unique. Once validated these colors are combined into a NxN table with N being the dimensions previously specified with all of the selected colors combined.</p>
