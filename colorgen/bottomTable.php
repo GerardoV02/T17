8b2817868a2c6c3fb1d21657af2f74f56d456fc0
Gerardo Valenzuela <gerardov@colostate.edu>
2024-04-04 19:51:26 -0600

    Updated table to be a variable

--- colorgen/bottomTable.php
+++ colorgen/bottomTable.php
@@ -4,42 +4,45 @@ if(isset($_POST["dimension"])){
     $dim = $_POST["dimension"];
     $num = $_POST["num_colors"];
 
-echo ('<table class = "BottomTable">');
+$table = '<table class = "BottomTable">';
 $letterNum = ord('A');
 
 for($i = 0; $i < $dim+1; $i++){
-    echo("<tr>");
+    $table.="<tr>";
+    //echo("<tr>");
 
     for($j = 0; $j < $dim+1;$j++){
 
         //Left uppermost empty
         if($i == 0 && $j == 0){
-            echo('<td class = "Cell"></td>');
+            $table.='<td class = "Cell"></td>';
             continue;
         }
 
         //Echos out letter for top rows
         if($i == 0){
             $letter = chr($letterNum);
-            echo("<td class = 'Cell'>$letter</td>");
+            $table.="<td class = 'Cell'>$letter</td>";
             $letterNum++;
             continue;
         }
 
         if($j == 0){
-            echo("<td class = 'Cell'>$i</td>");
+            $table.="<td class = 'Cell'>$i</td>";
             continue;
         }
 
-        echo('<td class = "Cell">');
+        $table.='<td class = "Cell">';
 
         //content here
 
-        echo("</td>");
+        $table.="</td>";
 
     }
-    echo("</tr>");
+    $table.="</tr>";
 }
 echo("</table>");
 }
+
+echo($table);
 ?>
