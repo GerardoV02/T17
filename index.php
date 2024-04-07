e9e03d7437382f9067c0eb7b2d1f7f7864601b5d
Andrew Partain <andrew.partain@colostate.edu>
2024-04-07 12:48:40 -0600

    Added colour pallet info and updated hover colour

--- index.php
+++ index.php
@@ -23,5 +23,15 @@ Classes:
 <h3 class="description">Description Of Our Prototype</h3>
 <p class="description of colorgenerator">Our color generator allows users to create a unique table based on there own inputs. First the user specifies the dimension and the number if unique colors they would like to include. Once this is set the user then selects the each color they would like to use.
 This input is validated to make sure there are no repeats as each color must be unique. Once validated these colors are combined into a NxN table with N being the dimensions previously specified with all of the selected colors combined.</p>
+
+<h3 class="PalletInfo">About The Colours</h3>
+<p class="description of colorgenerator">The colour pallet for this website was created using a random generator, then furthur tweaked until it reached adequate contrast ratios.</p>
+<p><strong>Colours used and their contrast against their main background:</strong></p>
+<ul>
+    <li><strong>Text & NavBar:</strong> <em>#FFFEFE</em> [Against Background: <a href="https://webaim.org/resources/contrastchecker/?fcolor=FFFEFE&bcolor=2E2C3D">13.51</a>]</li>
+    <li><strong>Accent:</strong> <em>#D6493B</em> [Against NavBar: <a href="https://webaim.org/resources/contrastchecker/?fcolor=D6493B&bcolor=FFFEFE">4.27</a>]</li>
+    <li><strong>Hover & Links:</strong> <em>#FEBB70</em> [Against Background: <a href="https://webaim.org/resources/contrastchecker/?fcolor=FEBB70&bcolor=2E2C3D">8.13</a>]</li>
+    <li><strong>Background:</strong> <em>#2E2C3D</em></li>
+
 </div>
 </html>
